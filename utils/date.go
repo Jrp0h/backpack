01cfd04f058b42cb19e3c150d91ac3c8e9b0e404
Marcus Nilsson <marcus.nilsson@genarp.com>
2022-04-22 00:18:04 +0200

    Utils: Made date formats padded

--- utils/date.go
+++ utils/date.go
@@ -7,14 +7,22 @@ import (
 )
 
 func FormatDate(format string, t time.Time) string {
-	result := strings.ReplaceAll(format, "%Y", fmt.Sprint(t.Year()))
-	result = strings.ReplaceAll(result, "%m", fmt.Sprintf("%d", t.Month()))
-	result = strings.ReplaceAll(result, "%d", fmt.Sprint(t.Day()))
+	result := strings.ReplaceAll(format, "%Y", pad2(t.Year()))
+	result = strings.ReplaceAll(result, "%m", pad2(int(t.Month())))
+	result = strings.ReplaceAll(result, "%d", pad2(t.Day()))
 
-	result = strings.ReplaceAll(result, "%H", fmt.Sprint(t.Hour()))
-	result = strings.ReplaceAll(result, "%M", fmt.Sprint(t.Minute()))
-	result = strings.ReplaceAll(result, "%S", fmt.Sprint(t.Second()))
+	result = strings.ReplaceAll(result, "%H", pad2(t.Hour()))
+	result = strings.ReplaceAll(result, "%M", pad2(t.Minute()))
+	result = strings.ReplaceAll(result, "%S", pad2(t.Second()))
 
 
 	return result
+}
+
+func pad2(num int) string {
+	if num < 10 {
+		return fmt.Sprintf("0%d", num)
+	}
+
+	return fmt.Sprint(num)
 }
