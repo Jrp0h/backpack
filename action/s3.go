8b6f07d47bd241b4785abec39af1928caf6e9b7a
Marcus Nilsson <marcus.nilsson@genarp.com>
2022-04-23 19:39:31 +0200

    Actions: Added ListFile and Fetch to s3

--- action/s3.go
+++ action/s3.go
@@ -3,6 +3,7 @@ package action
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/Jrp0h/backpack/utils"
 	"github.com/aws/aws-sdk-go/aws"
@@ -10,19 +11,20 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/google/uuid"
 )
 
 type s3Action struct {
-	bucket string
-	region string
-	clientID string
+	bucket       string
+	region       string
+	clientID     string
 	clientSecret string
-	token string
+	token        string
 }
 
 func (action *s3Action) createConnection() (*session.Session, error) {
 	sess, err := session.NewSession(&aws.Config{
-		Region: &action.region,
+		Region:      &action.region,
 		Credentials: credentials.NewStaticCredentials(action.clientID, action.clientSecret, action.token),
 	})
 
@@ -38,24 +40,24 @@ func (action *s3Action) CanValidateConnection() bool {
 }
 
 func (action *s3Action) TestConnection() error {
-	_, err := action.createConnection() 
+	_, err := action.createConnection()
 	return err
 }
 
 func (action *s3Action) Upload(fileData *utils.FileData) error {
-	session, err := action.createConnection() 
+	session, err := action.createConnection()
 
 	if err != nil {
 		return err
 	}
 
-	f, err  := os.Open(fileData.Path)
+	f, err := os.Open(fileData.Path)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	// Check if file with the same name already exists 
+	// Check if file with the same name already exists
 	_, err = s3.New(session).HeadObject(&s3.HeadObjectInput{
 		Bucket: &action.bucket,
 		Key:    &fileData.Name,
@@ -79,13 +81,51 @@ func (action *s3Action) Upload(fileData *utils.FileData) error {
 }
 
 func (action *s3Action) ListFiles() ([]string, error) {
-	// TODO: Implement
-	return nil, nil
+	session, err := action.createConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := s3.New(session).ListObjects(&s3.ListObjectsInput{
+		Bucket: &action.bucket,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	files := make([]string, 0)
+	for _, obj := range result.Contents {
+		files = append(files, *obj.Key)
+	}
+
+	return files, nil
 }
 
 func (action *s3Action) Fetch(file string) (string, error) {
-	// TODO: Implement
-	return "", nil
+	session, err := action.createConnection()
+
+	if err != nil {
+		return "", err
+	}
+
+	outPath := path.Join(os.TempDir(), uuid.NewString()+".zip")
+	outFile, err := os.OpenFile(outPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return "", err
+	}
+	defer outFile.Close()
+
+	uploader := s3manager.NewDownloader(session)
+	_, err = uploader.Download(outFile, &s3.GetObjectInput{
+		Bucket: &action.bucket,
+		Key:    &file,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return outPath, nil
 }
 
 func loadS3Action(data *map[string]string) (Action, error) {
